a26832b8737a88eb94877bda1848999d839421d7
tonyzl <germanantoniosiete@gmail.com>
2023-12-07 10:26:31 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tutotial-di2023';
+}
